89a70b59aa9443996b9fbaf35a9156a1ec598b0d
xaridar <xaridara@gmail.com>
2020-11-16 20:37:49 -0500

    restructure file system

--- src/main/java/Card_Game/JsonAccessor.java
+++ src/main/java/Card_Game/JsonAccessor.java
@@ -6,6 +6,7 @@ import Card_Game.Cards.Monster;
 import com.google.common.collect.Iterables;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import org.apache.commons.lang3.tuple.Pair;
 
@@ -16,29 +17,25 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
 public class JsonAccessor {
-
-    private static final String BASE_DIR = "carddata/jsons/";
-    private static final String CARD_DIR = BASE_DIR + "cards/";
-    private static final String DECK_DIR = BASE_DIR + "decks/";
 
     private static Gson gson;
 
 
     public static void fillDeck(String deckName, Deck deck, Player player) throws IOException {
         if (gson == null) gson = new Gson();
-        Reader reader = Files.newBufferedReader(Paths.get(DECK_DIR + deckName + ".json"));
+        Reader reader = Files.newBufferedReader(Paths.get(deckName));
         JsonObject object = gson.fromJson(reader, JsonObject.class);
         JsonArray array = object.getAsJsonArray("deck");
         array.iterator().forEachRemaining(jsonElement -> {
             if (jsonElement.isJsonPrimitive()) {
                 String fileName = jsonElement.getAsString().toLowerCase().replace(" ", "_") + ".json";
                 try {
-                    Reader fileReader = Files.newBufferedReader(Paths.get(CARD_DIR + fileName));
+                    Reader fileReader = Files.newBufferedReader(Paths.get(deckName.split("/decks/")[0] + "/cards/" + fileName));
                     JsonObject obj = gson.fromJson(fileReader, JsonObject.class);
                     if (obj.get("type").getAsString().equals("monster")) {
                         Monster monster = gson.fromJson(obj, Monster.class);
@@ -65,24 +62,57 @@ public class JsonAccessor {
 
     public static List<String> pickDecks() throws IOException {
         if (gson == null) gson = new Gson();
-        Reader reader = Files.newBufferedReader(Paths.get(BASE_DIR + "setdecks.json"));
+        Reader reader = Files.newBufferedReader(Paths.get("data/setup.json"));
         JsonObject object = gson.fromJson(reader, JsonObject.class);
         JsonArray array = object.getAsJsonArray("players");
         List<String> decks = new ArrayList<>();
         array.iterator().forEachRemaining(jsonElement -> {
             if (jsonElement.isJsonObject()) {
                 String name = ((JsonObject) jsonElement).get("deck").getAsString();
-                if (new File(DECK_DIR + name).exists()) decks.add(name); else decks.add(pickRandomDeck());
+                if (name.startsWith("%")) {
+                    name = name.substring(1);
+                    JsonArray dirArray = object.getAsJsonArray("deck_dirs");
+                    boolean found = false;
+                    for (JsonElement dir : dirArray) {
+                        if (dir.isJsonPrimitive()) {
+                            if (new File(dir.getAsString() + name + ".json").exists()) {
+                                decks.add(dir.getAsString() + name + ".json");
+                                found = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!found) {
+                        try {
+                            decks.add(pickRandomDeck());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                try {
+                    decks.add(pickRandomDeck());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
         return decks;
     }
 
-    private static String pickRandomDeck() {
+    private static String pickRandomDeck() throws IOException {
         Random random = new Random();
         List<String> possibleNames = new ArrayList<>();
-        File[] files = new File(DECK_DIR).listFiles();
-        if (files != null) Arrays.stream(files).forEach(file -> possibleNames.add(file.getName().split(".json")[0]));
+        Reader reader = Files.newBufferedReader(Paths.get("data/setup.json"));
+        JsonObject object = gson.fromJson(reader, JsonObject.class);
+        JsonArray array = object.getAsJsonArray("deck_dirs");
+        for (JsonElement dir : array) {
+            if (dir.isJsonPrimitive()) {
+                for (File file : Objects.requireNonNull(new File(dir.getAsString()).listFiles())) {
+                    possibleNames.add(dir.getAsString() + file.getName());
+                }
+            }
+        }
         return Iterables.get(possibleNames, random.nextInt(possibleNames.size()));
     }
 }
